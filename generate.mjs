9c31a4c21f4f05bc0ba3b7ffd25a8c51ac0562a0
Arvid Hoppe <ah@mc-kk.de>
2024-11-20 17:04:08 +0100

    Batman

---
+++ generate.mjs
@@ -0,0 +1,32 @@
+import * as Icons from "react-bootstrap-icons";
+import fs from "node:fs";
+
+const template = (iconName) => 
+`
+"use client";
+import React from "react";
+import {IconProps, ${iconName}} from "react-bootstrap-icons";
+
+export const CI_${iconName} : React.FC<IconProps> = ({...props}) => {
+    return (
+        <>
+            <${iconName} {...props}/>
+        </>
+    );
+}
+`;
+
+function generate() {
+
+    const iconNames = Object.keys(Icons).filter(name => name !== "__esModule" && name !== "default");
+    if(!fs.existsSync("icons/")) {
+        fs.mkdir("icons")
+    }
+        
+    iconNames.forEach(icon => {
+        fs.writeFileSync("icons/CI_"+icon+".tsx", template(icon));
+    })
+    
+}
+
+generate();
---
+++ readme.md
@@ -0,0 +1,4 @@
+# React Bootstrap Components for NextJS Client side
+
+## How to build
+Clone the repository and execute `node generate.mjs`
