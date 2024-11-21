a6bf35a7f368ce68dc003f6cd458db2bdf5a13c7
Arvid Hoppe <ah@mc-kk.de>
2024-11-21 09:55:30 +0100

    made props optional

--- icons/CI.tsx
+++ icons/CI.tsx
@@ -4,7 +4,7 @@ import * as Icons from "react-bootstrap-icons";
 import {IconProps} from "react-bootstrap-icons";
 import React from "react";
 
-export const CI : React.FC<{icon : string, props : IconProps}> = ({icon, props}) => {
+export const CI : React.FC<{icon : string, props? : IconProps}> = ({icon, props}) => {
     const keys = Object.keys(Icons)
     if(keys.includes(icon)) {
         const IconEl = icon;
